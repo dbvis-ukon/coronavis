189e1145168f77e18f2451944ac8aa2152191264
Daniel Seebacher <seebacher@dbvis.inf.uni-konstanz.de>
2019-10-29 14:46:55 +0100

    add database dockerfile and example dataset
    Former-commit-id: 84120895c034b671226eb17e5e10b4eb8305cf4d

---
+++ Database/import_dataset.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Starting import of dataset..."
+osm2pgsql --create --database gis --latlong --host localhost --username gis_user /importdata/baden-wuerttemberg-latest.osm.pbf
+echo "Finished import of dataset import of dataset..."
+echo "Database ready!"
