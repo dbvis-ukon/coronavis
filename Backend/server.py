99a14bb08d7b6dd7d6876228aa7debb8faf07172
Daniel Seebacher <seebacher@dbvis.inf.uni-konstanz.de>
2020-03-21 23:08:11 +0100

    allow cors
    Former-commit-id: f65aa382008806f8c7191db60d7e1efa1fd62c73

--- Backend/server.py
+++ Backend/server.py
@@ -3,6 +3,7 @@
 import atexit
 from flask import Flask, render_template, jsonify, make_response
 from flask_compress import Compress
+from flask_cors import CORS
 
 from corona_app.api import backend_api
 from corona_app.model import *
@@ -15,7 +16,8 @@ db.init_app(app)
 # Register Blueprints
 app.register_blueprint(backend_api)
 
-comp = Compress(app)
+CORS(app)
+Compress(app)
 
 # defining function to run on shutdown
 def close_running_threads():
