324298ec61b29ca4741e23219e319efe1989a7e3
Udo Schlegel <u.schlegel@uni-konstanz.de>
2020-03-22 11:13:42 +0100

    Map Data
    added map data crawler
    
    
    Former-commit-id: bab6a7d22327dcec33641da2388023bd1b1951d6

--- Crawler/crawler.py
+++ Crawler/crawler.py
@@ -1,10 +1,6 @@
 """
 DIVI - Hospitals with their capacities
 https://www.divi.de/register/intensivregister?view=items
-
-DIVI - Beds with intensive care
-https://www.divi.de/register/kartenansicht
-
 """
 import time
 import pandas
@@ -32,7 +28,7 @@ def legends(class_input):
 def get_html_content(url, data):
     data = urllib.parse.urlencode(data)
     data = data.encode('utf-8')
-    page = urllib.request.urlopen(quote_page, data)
+    page = urllib.request.urlopen(url, data)
 
     html_data = page.read()
     return html_data
@@ -45,16 +41,19 @@ def remove_spaces(text):
     return text
 
 
-if __name__ == "__main__":
+def get_geo_location(adress):
+    geolocator = Nominatim(user_agent='COVID-19', timeout=10)
+    location = geolocator.geocode(adress)
+    loc = (None, None)
+    if location != None:
+        loc = (location.latitude, location.longitude)
+        
+    return loc, location
 
-    quote_page = 'https://www.divi.de/register/intensivregister?view=items'
-    values = {
-        'list': {
-            'limit': 0
-        }
-    }
 
-    html_data = get_html_content(quote_page, values)
+def crawl_webpage(url, data):
+    
+    html_data = get_html_content(url, data)
 
     soup = BeautifulSoup(html_data, 'html.parser')
 
@@ -83,29 +82,54 @@ if __name__ == "__main__":
                 if tmp == '' and td.span != None:
                     tmp = ' '.join(td.span.get('class'))
                     tmp = legends(tmp)
+                if td.find('a'):
+                    tmp += ' ' + td.find('a').get('href')
                 hospital_entry.append(tmp)
             
         hospital_entries.append(hospital_entry)
         
-    print(hospital_entries[0])
-        
+    return hospital_entries
+
+
+def get_hospital_geo_locations(hospital_entries):
     len_ = len(hospital_entries)
     for i, hospital_entry in enumerate(hospital_entries):
         adress = hospital_entry[1]
         print(str(i + 1) + ' / ' + str(len_))
         print('Crawled location: ' + adress)
         
-        geolocator = Nominatim(user_agent='COVID-19')
-        location = geolocator.geocode(adress)
-        loc = (None, None)
-        if location != None:
-            loc = (location.latitude, location.longitude)
-        print('Found location: ' + str(location))
+        try:
+            loc, location = get_geo_location(adress)
+            print('Found location: ' + str(location))
+            
+        except Exception as e:
+            loc = (None, None)
+            print('Error ' + str(e))
+            
         hospital_entry.append(loc)
         hospital_entries[i] = hospital_entry
         
         if i % 10:
             time.sleep(1)
+            
+    return hospital_entries
+
+
+if __name__ == "__main__":
+
+    quote_page = 'https://www.divi.de/register/intensivregister?view=items'
+    values = {
+        'list': {
+            'limit': 0
+        }
+    }
+
+    hospital_entries = crawl_webpage(quote_page, values)
+        
+    print(hospital_entries[0])
+        
+    hospital_entries = get_hospital_geo_locations(hospital_entries)
 
     df = pandas.DataFrame(hospital_entries, columns=['Name', 'Adress', 'String', 'Kontakt', 'Bundesland', 'ICU low care', 'ICU high care', 'ECMO', 'Stand', 'Location'])
+    df['Stand'] =  pandas.to_datetime(df['Stand'], format='%d.%m.%Y %H:%M')
     df.to_csv('rki_hospitals.csv')
