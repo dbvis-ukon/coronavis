87ec45c74433560e8b45a5d415f7c2bdc666a69b
Jochen Görtler <web@goertler.email>
2020-03-22 16:46:24 +0100

    First version of glyphs
    Former-commit-id: 3355c51e66c06beed21b470e4beb3a520c28738b

--- Frontend/gis/src/app/map/overlays/simple-glyph.layer.ts
+++ Frontend/gis/src/app/map/overlays/simple-glyph.layer.ts
@@ -1,12 +1,12 @@
-import {FeatureCollection} from 'geojson';
+import { FeatureCollection } from 'geojson';
 
 import * as L from 'leaflet';
 import * as d3 from 'd3';
-import {Overlay} from './overlay';
-import {TooltipService} from 'src/app/services/tooltip.service';
-import {TooltipDemoComponent} from 'src/app/tooltip-demo/tooltip-demo.component';
-import {DiviHospital} from 'src/app/services/divi-hospitals.service';
-import {LatLng} from "leaflet";
+import { Overlay } from './overlay';
+import { TooltipService } from 'src/app/services/tooltip.service';
+import { TooltipDemoComponent } from 'src/app/tooltip-demo/tooltip-demo.component';
+import { DiviHospital } from 'src/app/services/divi-hospitals.service';
+import { LatLng } from "leaflet";
 
 export class SimpleGlyphLayer extends Overlay {
 
@@ -19,7 +19,7 @@ export class SimpleGlyphLayer extends Overlay {
     // .domain expects an array of [min, max] value
     // d3.extent returns exactly this array
     const colorScale = d3.scaleOrdinal<string, string>().domain(['Verfügbar', 'Begrenzt', 'Ausgelastet', 'Nicht verfügbar'])
-      .range(['green', 'yellow', 'red', 'black']);
+      .range(['rgb(113,167,133)', 'rgb(230,181,72)', 'rgb(198,106,75)', '#bbbbbb']);
 
     const locationPoints = this.data.map(d => this.map.latLngToContainerPoint(d.Location));
     const [xMin, xMax] = d3.extent(locationPoints, d => d.x);
@@ -51,21 +51,20 @@ export class SimpleGlyphLayer extends Overlay {
 
 
 
-
     const gHostpitals = d3.select(svgElement)
       .selectAll('g.hospital')
       .data<DiviHospital>(this.data)
       .enter()
       .append<SVGGElement>('g')
       .attr('class', 'hospital')
-        .attr('transform', d => {
-          const p = this.map.latLngToLayerPoint(d.Location);
-          console.log(p, d.Location);
-          return `translate(${p.x}, ${p.y})`})
+      .attr('transform', d => {
+        const p = this.map.latLngToLayerPoint(d.Location);
+        console.log(p, d.Location);
+        return `translate(${p.x}, ${p.y})`
+      })
       .on('mouseenter', d1 => {
-        console.log('mouseenter', d1);
         const evt: MouseEvent = d3.event;
-        const t = this.tooltipService.openAtElementRef(TooltipDemoComponent, {x: evt.clientX, y: evt.clientY}, [
+        const t = this.tooltipService.openAtElementRef(TooltipDemoComponent, { x: evt.clientX, y: evt.clientY }, [
           {
             overlayX: 'start',
             overlayY: 'top',
@@ -105,24 +104,49 @@ export class SimpleGlyphLayer extends Overlay {
 
     const rectSize = 10;
 
+    const padding = 2;
+    const yOffset = 10;
+
+    gHostpitals
+      .append('rect')
+      .attr('width', '50')
+      .attr('height', '22')
+      .attr('fill', 'white')
+      .attr('stroke', '#cccccc');
+
+    let hospitalName = gHostpitals
+      .append('text')
+      .text(d1 => {
+        // Hackity hack :)
+        const splitted = d1.Adress.split(" ");
+        return splitted[splitted.length - 1]
+      })
+      .attr('x', padding)
+      .attr('y', '8')
+      .attr('font-size', '8px');
+
     gHostpitals
       .append('rect')
       .attr('width', `${rectSize}px`)
       .attr('height', `${rectSize}px`)
+      .attr('x', padding)
+      .attr('y', yOffset)
       .style('fill', d1 => colorScale(d1.icuLowCare));
 
     gHostpitals
       .append('rect')
       .attr('width', `${rectSize}px`)
       .attr('height', `${rectSize}px`)
-      .attr('x', `${rectSize}px`)
+      .attr('y', yOffset)
+      .attr('x', `${rectSize + padding * 2}px`)
       .style('fill', d1 => colorScale(d1.icuHighCare));
 
     gHostpitals
       .append('rect')
       .attr('width', `${rectSize}px`)
       .attr('height', `${rectSize}px`)
-      .attr('x', `${2 * rectSize}px`)
+      .attr('y', yOffset)
+      .attr('x', `${2 * rectSize + padding * 3}px`)
       .style('fill', d1 => colorScale(d1.ECMO));
 
 
@@ -135,7 +159,7 @@ export class SimpleGlyphLayer extends Overlay {
     const latExtent = d3.extent(this.data, i => i.Location.lat);
     const lngExtent = d3.extent(this.data, i => i.Location.lng);
 
-    return L.svgOverlay(svgElement, [[latExtent[0], lngExtent[0]], [latExtent[1], lngExtent[1]]], {interactive:true});
+    return L.svgOverlay(svgElement, [[latExtent[0], lngExtent[0]], [latExtent[1], lngExtent[1]]], { interactive: true });
     // return L.svgOverlay(svgElement, this.map.getBounds());
   }
 }
