fb5c19f11ecb2e58a845bd1af96606bfd92d80cf
Udo Schlegel <u.schlegel@uni-konstanz.de>
2020-03-23 23:45:23 +0100

    Landkreis, Bundesland Data
    added new landkreis data,
    added new bundesland data
    
    
    Former-commit-id: e834b3b858a120819f236d51b10109f5ea915a9f

--- Crawler/crawler_rki_dashboard.py
+++ Crawler/crawler_rki_dashboard.py
@@ -4,11 +4,10 @@ https://experience.arcgis.com/experience/478220a4c454480e823b17327b2bf1d4/page/p
 Landkreise
 https://experience.arcgis.com/experience/478220a4c454480e823b17327b2bf1d4/page/page_1/
 """
-import re
 import time
 import pandas
 
-from requests_html import HTMLSession
+import requests
 
 import db
 
@@ -18,55 +17,93 @@ def remove_spaces(text):
     text = text.strip()
     text = ' '.join(text.split())
     return text
-
-
-def crawl_webpage(url):
-
-    session = HTMLSession()
-    r = session.get(url)
-    r.html.render(sleep=10, keep_page=True)
-
+    
+    
+def full_fetch_lk(url):
+    
+    r = requests.get(url)
+    rj = r.json()
+    
     entries = []
-    for entry in r.html.find('.feature-list', first=True).find('.feature-list-item'):
-        data = re.split(r'\s{2,}', entry.text.replace(u'\xa0', u' ').replace(',', ''))
-        entries.append(data)
+    for data in rj['features']:
+        d = data['attributes']
+        entry = [
+            d['GEN'],
+            d['BEZ'],
+            d['BL'],
+            d['county'],
+            d['cases'],
+            d['deaths'],
+            d['EWZ'],
+            d['death_rate'],
+            d['cases_per_100k'],
+            d['cases_per_population']
+        ]
+        entries.append(entry)
     
-    return entries
-    
-    
-def full_fetch(quote_page):
-    entries = crawl_webpage(quote_page)
+    json_str = str(rj)
 
-    df = pandas.DataFrame(entries, columns=['cases', 'deaths', 'name'])
+    df = pandas.DataFrame(entries, columns=['name', 'desc', 'bl', 'county', 'cases', 'deaths', 'population', 'death_rate', 'cases_per_100k', 'cases_per_population'])
     df['cases'] = df['cases'].astype('int')
     df['deaths'] = df['deaths'].astype('int')
+    df['population'] = df['population'].astype('int')
+    df['death_rate'] = df['death_rate'].astype('float')
+    df['cases_per_100k'] = df['cases_per_100k'].astype('float')
+    df['cases_per_population'] = df['cases_per_population'].astype('float')
     
-    return df
+    return df, json_str
+
+
+def full_fetch_bl(url):
+    
+    r = requests.get(url)
+    rj = r.json()
+    
+    entries = []
+    for data in rj['features']:
+        d = data['attributes']
+        entry = [
+            d['LAN_ew_GEN'],
+            d['LAN_ew_BEZ'],
+            d['Fallzahl'],
+            d['Death'],
+            d['LAN_ew_EWZ']
+        ]
+        entries.append(entry)
+    
+    json_str = str(rj)
+
+    df = pandas.DataFrame(entries, columns=['name', 'desc', 'cases', 'deaths', 'population'])
+    df['cases'] = df['cases'].astype('int')
+    df['deaths'] = df['deaths'].astype('int')
+    df['population'] = df['population'].astype('int')
+    
+    return df, json_str
 
 
 if __name__ == "__main__":
     
     print('Start ArcGIS Crawling')
 
-    quote_page = 'https://npgeo-de.maps.arcgis.com/apps/opsdashboard/index.html#/bca904a683844e7784141559b540dbc2'
+    quote_page = 'https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=cases%20desc&outSR=102100&resultOffset=0&resultRecordCount=1000&cacheHint=true'
 
-    df = full_fetch(quote_page)
+    df, json_str = full_fetch_lk(quote_page)
     
     for index, row in df.iterrows():
-        covid19caseslk = db.Covid19CasesLK(**row.to_dict())
-        print(covid19caseslk)
-        db.sess.add(covid19caseslk)
+        caseslk = db.CasesLK(**row.to_dict())
+        print(caseslk)
+        db.sess.add(caseslk)
 
     db.sess.commit()
     
-    quote_page = 'https://npgeo-de.maps.arcgis.com/apps/opsdashboard/index.html#/2694322fc7894bf5886647f652f093ca'
+    quote_page = 'https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/Coronaf%C3%A4lle_in_den_Bundesl%C3%A4ndern/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Fallzahl%20desc&resultOffset=0&resultRecordCount=25&cacheHint=true'
 
-    df = full_fetch(quote_page)
+    df, json_str = full_fetch_bl(quote_page)
     
     for index, row in df.iterrows():
-        covid19casesbl = db.Covid19CasesBL(**row.to_dict())
-        print(covid19casesbl)
-        db.sess.add(covid19casesbl)
+        casesbl = db.CasesBL(**row.to_dict())
+        print(casesbl)
+        db.sess.add(casesbl)
 
     db.sess.commit()
     
