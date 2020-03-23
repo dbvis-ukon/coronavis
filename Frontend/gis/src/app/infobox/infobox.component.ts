1559d1f2bf5b95d81bcd8510f1e6b091b8567d85
Matthias Kraus <kraus.matze@googlemail.com>
2020-03-23 19:00:56 +0100

    infobox - make toggleable
    Former-commit-id: 4c91238bcf4f4837217bb66bd2eb25e9fc496353

--- Frontend/gis/src/app/infobox/infobox.component.ts
+++ Frontend/gis/src/app/infobox/infobox.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class InfoboxComponent implements OnInit {
 
   constructor() { }
+  
+  infoboxExtended = true;
 
   ngOnInit(): void {
   }
