e56123af50e72c360fede9aead4cd28f2bb27541
Daniel Seebacher <seebacher@dbvis.inf.uni-konstanz.de>
2020-03-26 18:26:01 +0100

    add health check for database connection
    Former-commit-id: b3451d1f2defb8d4ca627bd2e95c3592d6541246

--- Backend/views/health.py
+++ Backend/views/health.py
@@ -1,8 +1,23 @@
-from flask import Blueprint
+import logging
+
+from flask import Blueprint, jsonify
+
+from db import db
 
 routes = Blueprint('health', __name__, url_prefix='/health')
 
-@routes.route('')
+@routes.route('/')
 def healthcheck():
-    # FIXME: the database connection should be checked here!
-    return "ok", 200
+
+    is_database_working = True
+    output = 'database is ok'
+
+    try:
+        # to check database we will execute raw query
+        db.engine.execute('select 1 as is_alive;').fetchall()
+    except Exception as e:
+        output = 'database is not ok, see logs for more information'
+        is_database_working = False
+        logging.error(str(e))
+
+    return jsonify((is_database_working, output)), 200 if is_database_working else 500
