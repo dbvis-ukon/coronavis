e82ddbe4a52c97b55e060a6cc8d3df9167f0287e
Daniel Seebacher <seebacher@dbvis.inf.uni-konstanz.de>
2020-03-26 14:12:17 +0100

    use environment variables for database connection if available
    Former-commit-id: 55adcda7755d5e82ca2ef404f04d0e0d782dd229

--- Backend/server.py
+++ Backend/server.py
@@ -1,16 +1,41 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template, jsonify, make_response
+import logging
+import os
+from urllib.parse import quote
+
+from flask import Flask
 from flask_compress import Compress
 from flask_cors import CORS
 
+import config
 from corona_app.api import backend_api
-from corona_app.model import *
 from corona_app.api import cache
+from corona_app.model import *
+
+logger = logging.getLogger(__name__)
 
 # Create Flask application
 app = Flask(__name__)
-app.config.from_pyfile('config.py')
+
+# create postgresql connection string
+try:
+    DB_HOST = os.environ.get('DB_HOST')
+    DB_PORT = os.environ.get('DB_PORT')
+    DB_USER = os.environ.get('DB_USER')
+    DB_PASS = os.environ.get('DB_PASS')
+    DB_NAME = os.environ.get('DB_NAME')
+
+    if None in (DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME):
+        raise KeyError
+    else:
+        DB_CONNECTION_STRING = f"postgresql://{quote(DB_USER)}:{quote(DB_PASS)}@{quote(DB_HOST)}:{quote(DB_PORT)}/{quote(DB_NAME)}"
+except KeyError as e:
+    logger.warning('One or multiple necessary environment variables not set, using config.py file as backup')
+    DB_CONNECTION_STRING = config.SQLALCHEMY_DATABASE_URI
+
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_CONNECTION_STRING
+
 db.init_app(app)
 cache.init_app(app)
 
