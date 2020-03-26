48407c78d301062b56ff1413c2ba57a4df68e7c8
Daniel Seebacher <seebacher@dbvis.inf.uni-konstanz.de>
2020-03-26 20:47:28 +0100

    add logging of pw, remove if successful
    Former-commit-id: 09577366b3ab5ab309bed1f3b4d14af65e3afd97

--- Backend/server.py
+++ Backend/server.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import os
-from urllib.parse import quote
+import logging
 
 from flask import Flask
 from flask_compress import Compress
@@ -25,12 +25,18 @@ try:
     DB_PASS = os.environ.get('DB_PASS')
     DB_NAME = os.environ.get('DB_NAME')
 
+
+
     if None in (DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME):
         raise KeyError
     else:
-        DB_CONNECTION_STRING = f"postgresql://{quote(DB_USER)}:{quote(DB_PASS)}@{quote(DB_HOST)}:{quote(DB_PORT)}/{quote(DB_NAME)}"
+        DB_CONNECTION_STRING = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+    logging.warning(DB_CONNECTION_STRING)
+    
 except KeyError as e:
-    logger.warning('One or multiple necessary environment variables not set, using config.py file as backup')
+    logging.warning('One or multiple necessary environment variables not set, using config.py file as backup')
+    #DB_CONNECTION_STRING = config.SQLALCHEMY_DATABASE_URI
 
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_CONNECTION_STRING
 
