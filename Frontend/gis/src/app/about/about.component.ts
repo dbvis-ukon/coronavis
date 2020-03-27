aed659ab6e4f904fdc18b4195402283356ae2526
Wolfgang Jentner <wolfgang.jentner@uni-konstanz.de>
2020-03-27 16:00:05 +0100

    [#125] wip version numbers
    Former-commit-id: 865c711870e964ffefdba0d7647339b600241a36

--- Frontend/gis/src/app/about/about.component.ts
+++ Frontend/gis/src/app/about/about.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { DiviHospital } from '../services/divi-hospitals.service';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-about',
@@ -9,10 +11,25 @@ import { DiviHospital } from '../services/divi-hospitals.service';
 })
 export class AboutComponent implements OnInit {
 
+  frontendVersion: string;
+
+  apiVersion: string;
+
+  tileServerVersion: string;
+
   constructor(
-    public dialogRef: MatDialogRef<AboutComponent>) {}
+    public dialogRef: MatDialogRef<AboutComponent>,
+    private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.frontendVersion = environment.version;
+
+    this.http.get(
+      `${environment.apiUrl}version`, { responseType: 'text'} )
+    .subscribe(v => {
+      this.apiVersion = v;
+    });
+
   }
 
 
