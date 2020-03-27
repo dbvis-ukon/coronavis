4b066fb0efbe6906a1966a218615fb299cca62c0
Udo Schlegel <u.schlegel@uni-konstanz.de>
2020-03-27 14:07:31 +0100

    Code Review
    Former-commit-id: a97205cd5ad228e692bd8eaf19cc172db1eb2ea6

--- Crawler/crawl_divi_data.py
+++ Crawler/crawl_divi_data.py
@@ -14,7 +14,9 @@ import requests
 
 from db_config import SQLALCHEMY_DATABASE_URI
 
+
 print('Crawler for DIVI Data')
+
 
 def get_connection():
     conn = pg.connect(SQLALCHEMY_DATABASE_URI)
@@ -26,13 +28,13 @@ def get_connection():
 URL = "https://diviexchange.z6.web.core.windows.net"
 
 
-
 print('Fetch data')
 response = requests.get(URL)
 html = response.text
 regex = r"\"datasets\": ({.+?), {\"mode\": \"vega-lite\"}\)"
 match = re.findall(regex, html, re.DOTALL)
 data = json.loads(match[0][0:-1])
+
 
 def rep(e):
     d = {}
@@ -47,30 +49,42 @@ print('entries hospitals', len(entries))
 
 
 aquery = 'INSERT INTO divi_hospital_capacities_and_cases(datenbestand, gemeindeschluessel, ort, bundeslandschluessel, plz, webaddresse, icu_low_care_frei, icu_low_care_belegt, icu_low_care_einschaetzung, icu_low_care_in_24h, icu_high_care_frei, icu_high_care_belegt, icu_high_care_einschaetzung, icu_high_care_in_24h, icu_ecmo_care_frei, icu_ecmo_care_belegt, icu_ecmo_care_einschaetzung, icu_ecmo_care_in_24h, ecmo_faelle_jahr, covid19_aktuell, covid19_kumulativ, covid19_beatmet, covid19_verstorben, geom) VALUES %s'
+
 try:  
     conn, cur = get_connection()
-    cur.execute("Select Max(datenbestand) from divi_hospital_capacities_and_cases")
+
+    cur.execute("SELECT Max(datenbestand) from divi_hospital_capacities_and_cases")
     last_update = cur.fetchone()[0]
+
     print("db data version:", last_update)
     print("fetched data version:", datetime.datetime.now())
     print('Insert new data into DB...')
+
     psycopg2.extras.execute_values (
     cur, aquery, entries, template='(\''+ str(datetime.datetime.now()) + '\', %(gemeindeschluessel)s, %(ort)s, %(bundesland)s, %(plz)s, %(webadresse)s, %(icu_low_care_frei)s, %(icu_low_care_belegt)s, %(icu_low_care_einschätzung)s, %(icu_low_care_in_24_h_anzahl)s, %(icu_high_care_frei)s, %(icu_high_care_belegt)s, %(icu_high_care_einschätzung)s, %(icu_high_care_in_24_h_anzahl)s, %(icu_ecmo_frei)s, %(icu_ecmo_belegt)s, %(icu_ecmo_care_einschätzung)s, %(icu_ecmo_care_in_24_h_anzahl)s, %(anzahl_ecmofälle_pro_jahr)s, %(covid19_aktuell)s, %(covid19_kumulativ)s, %(covid19_beatmet)s, %(covid19_verstorben)s, ST_SetSRID( ST_Point(%(lon)s, %(lat)s), 4326))', page_size=500
 )
     conn.commit()
+
     print('Success')
     if(conn):
         cur.close()
         conn.close()
+
     exit(0)    
-except (Exception, pg.DatabaseError) as error :
+
+except (Exception, pg.DatabaseError) as error:
+    conn, cur = get_connection()
+
     print(error)
     print("Error in transction - Reverting all other operations of a transction")
     print("Most likely a simultanious update was applied faster.")
+
     conn.rollback()
+
     if(conn):
         cur.close()
-        conn.close()   
+        conn.close()
+
     exit(1)   
 
 
--- Crawler/crawl_rki_cases.py
+++ Crawler/crawl_rki_cases.py
@@ -12,7 +12,9 @@ import requests
 
 from db_config import SQLALCHEMY_DATABASE_URI
 
+
 print('Crawler for RKI detailed case data')
+
 
 def get_connection():
     conn = pg.connect(SQLALCHEMY_DATABASE_URI)
@@ -24,8 +26,9 @@ def get_connection():
 URL = "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_COVID19/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Meldedatum%20asc&resultOffset={}&resultRecordCount=2000&cacheHint=true"
 
 
-
 print('Fetch data')
+
+
 data = None
 has_data = True
 offset = 0
@@ -44,6 +47,8 @@ data = [d['attributes'] for d in data['features']]
 
 
 print('Parse data')
+
+
 entries = []
 for el in data:
     cc = ['case' for i in range(el['AnzahlFall'])]
@@ -69,32 +74,44 @@ print('current cases', len(entries))
 aquery = 'INSERT INTO cases(datenbestand, idbundesland, bundesland, landkreis, idlandkreis, objectid, meldedatum, gender, agegroup, casetype) VALUES %s'
 try:  
     conn, cur = get_connection()
+
     cur.execute("Select Max(datenbestand) from cases")
     last_update = cur.fetchone()[0]
     current_update = entries[0]['datenbestand'].replace(tzinfo=datetime.timezone(datetime.timedelta(hours=+1)))
+
     print("db data version:", last_update)
     print("fetched data version:", current_update)
+
     if last_update is not None and abs((current_update - last_update).total_seconds()) <= 2*60*60:
         print("No new data available (+/- 2h), skip update")
     else:
         print('Insert new data into DB (takes 2-5 seconds)...')
+
         psycopg2.extras.execute_values (
             cur, aquery, entries, template='(%(datenbestand)s, %(idbundesland)s, %(bundesland)s, %(landkreis)s, %(idlandkreis)s, %(objectid)s, %(meldedatum)s, %(gender)s, %(agegroup)s, %(casetype)s)', page_size=500
         )
         conn.commit()
+
         print('Success')
+
         if(conn):
             cur.close()
             conn.close()
+
         exit(0)    
-except (Exception, pg.DatabaseError) as error :
+except (Exception, pg.DatabaseError) as error:
+    conn, cur = get_connection()
+
     print(error)
     print("Error in transction - Reverting all other operations of a transction")
     print("Most likely a simultanious update was applied faster.")
+
     conn.rollback()
+
     if(conn):
         cur.close()
         conn.close()   
+
     exit(1)   
 
 
