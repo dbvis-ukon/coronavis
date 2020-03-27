39d12da9479ee5bb17131da02814ed93b6e5a8da
Udo Schlegel <u.schlegel@uni-konstanz.de>
2020-03-27 10:08:29 +0100

    Update db_config.py
    Former-commit-id: 552756c4c762cc68de99b2ab5a0045a982b330af

--- Crawler/db_config.py
+++ Crawler/db_config.py
@@ -1,5 +1,5 @@
 DATABASE_FILE = 'corona_app'
-SQLALCHEMY_DATABASE_URI = 'postgresql://corona:KbN%7CrjlN5f%2F.QU8EfP@db.dbvis.de/coronadb'  # + DATABASE_FILE
+SQLALCHEMY_DATABASE_URI = 'postgresql://' # 'postgresql://corona:KbN%7CrjlN5f%2F.QU8EfP@db.dbvis.de/coronadb'  # + DATABASE_FILE
 SQLALCHEMY_ECHO = True
 
 import os
