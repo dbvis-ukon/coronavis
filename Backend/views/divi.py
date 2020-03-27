4bb4de87e1131cb49d3ec0860f93ceced06ddbcc
Daniel Seebacher <seebacher@dbvis.inf.uni-konstanz.de>
2020-03-27 16:47:59 +0100

    add multiple endpoints for divi data
    Former-commit-id: 0a92f14f5690aa443bcc3a65331d75c54f0ad67f

---
+++ Backend/views/divi.py
@@ -0,0 +1,59 @@
+from flask import Blueprint, jsonify
+
+from cache import cache
+from db import db
+from models.divi import HospitalDevelopment, HospitalsDevelopmentPerLandkreis, HospitalsDevelopmentPerBundesland, \
+    HospitalsDevelopmentPerRegierungsbezirk
+from views.helpers import __as_feature_collection
+
+routes = Blueprint('divi', __name__, url_prefix='/divi')
+
+
+@routes.route('current', methods=['GET'])
+@cache.cached()
+def get_hospitals_current():
+    """
+        Return all Hospitals Developments
+    """
+    hospitals = db.session.query(HospitalDevelopment).all()
+    return jsonify(__as_feature_collection(hospitals)), 200
+
+
+@routes.route('development', methods=['GET'])
+@cache.cached()
+def get_hospitals_development():
+    """
+        Return all Hospitals Developments
+    """
+    hospitals = db.session.query(HospitalDevelopment).all()
+    return jsonify(__as_feature_collection(hospitals)), 200
+
+
+@routes.route('development/landkreise', methods=['GET'])
+@cache.cached()
+def get_hospitals_development_landkreise():
+    """
+        Return all Hospitals Developments
+    """
+    hospitals = db.session.query(HospitalsDevelopmentPerLandkreis).all()
+    return jsonify(__as_feature_collection(hospitals)), 200
+
+
+@routes.route('development/regierungsbezirke', methods=['GET'])
+@cache.cached()
+def get_hospitals_development_regierungsbezirke():
+    """
+        Return all Hospitals Developments
+    """
+    hospitals = db.session.query(HospitalsDevelopmentPerRegierungsbezirk).all()
+    return jsonify(__as_feature_collection(hospitals)), 200
+
+
+@routes.route('development/bundeslaender', methods=['GET'])
+@cache.cached()
+def get_hospitals_development_bundeslaender():
+    """
+        Return all Hospitals Developments
+    """
+    hospitals = db.session.query(HospitalsDevelopmentPerBundesland).all()
+    return jsonify(__as_feature_collection(hospitals)), 200
