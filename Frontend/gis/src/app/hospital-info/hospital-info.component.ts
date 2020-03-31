f7ae41f6585fffef90460bae9638ed161dd8a6a3
rita_temp <rita.sevastjanova@uni-konstanz.de>
2020-03-31 14:10:05 +0200

    [#185] show development for single hospitals in a stream graph
    Former-commit-id: ce1e2dac2cd150b85b1d5bcbabcf798a51eff826

--- Frontend/gis/src/app/hospital-info/hospital-info.component.ts
+++ Frontend/gis/src/app/hospital-info/hospital-info.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { DiviHospital } from '../services/divi-hospitals.service';
 import { ColormapService } from '../services/colormap.service';
+import * as d3 from "d3";
 
 @Component({
   selector: 'app-hospital-info',
@@ -18,6 +19,31 @@ export class HospitalInfoComponent implements OnInit {
   mode: 'dialog' | 'tooltip';
   @Input()
   data: DiviHospital
+
+  templateSpec = {
+    "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
+    "width": 300, "height": 50,
+    "data": {"values":[
+      ]},
+    "mark": {"type": "area", "interpolate": "step"},
+    "encoding": {
+      "x": {
+        "field": "Datum", "type": "temporal",
+        "axis": {"domain": false, "format": "%Y-%m-%d", "tickSize": 0}
+      },
+      "y": {
+        "field": "num", "type": "quantitative",
+        "axis": null,
+        "stack": "center"
+      },
+     "color": {"field":"Kategorie", "scale":{"domain": [], "range": []}}
+    }
+  };
+
+  specs = [];
+
+  bedAccessors = ['icu_low_care', 'icu_high_care', 'ecmo_state'];
+  bedAccessorsMapping = {'icu_low_care': 'ICU - Low Care', 'icu_high_care': 'ICU - High Care', 'ecmo_state': 'ECMO'};
 
 
 
@@ -39,6 +65,67 @@ export class HospitalInfoComponent implements OnInit {
 
       this.contactMsg = this.data.Kontakt;
     }
+
+    var data = [{"development" : {"timestamp" : "2020-03-27T14:49:00", "icu_low_care" : {"Begrenzt" : 1}, "icu_high_care" : {"Verfügbar" : 1}, "ecmo_state" : {"Nicht verfügbar" : 1}}}, {"development" : {"timestamp" : "2020-03-28T09:42:00", "icu_low_care" : {"Verfügbar" : 1}, "icu_high_care" : {"Verfügbar" : 1}, "ecmo_state" : {"Nicht verfügbar" : 1}}}, {"development" : {"timestamp" : "2020-03-29T10:38:00", "icu_low_care" : {"Verfügbar" : 1}, "icu_high_care" : {"Verfügbar" : 1}, "ecmo_state" : {"Nicht verfügbar" : 1}}}, {"development" : {"timestamp" : "2020-03-30T09:18:00", "icu_low_care" : {"Verfügbar" : 1}, "icu_high_care" : {"Begrenzt" : 1}, "ecmo_state" : {"Nicht verfügbar" : 1}}}, {"development" : {"timestamp" : "2020-03-31T09:04:00", "icu_low_care" : {"Begrenzt" : 1}, "icu_high_care" : {"Verfügbar" : 1}, "ecmo_state" : {"Nicht verfügbar" : 1}}}];
+    const bedStati = ['Verfügbar', 'Begrenzt', 'Ausgelastet']; //FIXME add "Nicht verfügbar" if should be displayed
+
+    var colors = [];
+    for (const bedStatus of bedStati) {
+     colors.push(this.getCapacityStateColor(bedStatus));
+    }
+
+    this.specs = [];
+    let maxNum = 0;
+
+    for(const bedAccessor of this.bedAccessors) {
+      let summedbedcounts = 0;
+      const dataValues = [];
+
+      for( const d of data) {
+        const development = d.development;
+
+        // fill the data object
+        for (const bedStatus of bedStati) {
+          const v = development[bedAccessor][bedStatus] || 0;
+          if(development[bedAccessor][bedStatus]) {
+            summedbedcounts++;
+          }
+
+          dataValues.push(
+            {
+              Kategorie: bedStatus,
+              num: v,
+              color: this.getCapacityStateColor(bedStatus),
+              Datum: development.timestamp
+            }
+          );
+          if (v > maxNum) {
+            maxNum = v;
+          }
+        }
+      }
+
+
+      // hack deep clone spec
+      const spec = JSON.parse(JSON.stringify(this.templateSpec));
+
+      // inject data values
+      spec.data.values = dataValues;
+
+      // also overwrite the title
+      spec.encoding.x.title = this.bedAccessorsMapping[bedAccessor];
+
+      if(summedbedcounts > 0) {
+        this.specs.push(spec);
+      }
+    }
+
+    // set the max value
+    this.specs.forEach(spec => {
+      spec.encoding.color.scale.domain = bedStati;
+      spec.encoding.color.scale.range = colors;
+      //spec.encoding.color.range = Math.min(maxNum+1, 5);
+    });
   }
 
   getCapacityStateColor(capacityState: string): string {
