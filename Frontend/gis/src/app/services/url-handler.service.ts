ed470cc473ef194e195072a84ecf7f3e605a6b09
Wolfgang Jentner <wolfgang.jentner@uni-konstanz.de>
2020-04-11 20:28:37 +0200

    [#266] import lzma directly
    Former-commit-id: cad869afb2ebe9142a0beb90cf039148c3bbf06a

--- Frontend/gis/src/app/services/url-handler.service.ts
+++ Frontend/gis/src/app/services/url-handler.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import * as JsonUrl from 'json-url/dist/browser/json-url-single.js';
+import 'json-url/dist/node/codecs';
+import 'lzma/bin/lzma';
 import { APP_CONFIG_URL_KEY, MAP_LOCATION_SETTINGS_URL_KEY } from 'src/constants';
 import { MapLocationSettings } from '../map/options/map-location-settings';
 import { MapOptions } from '../map/options/map-options';
