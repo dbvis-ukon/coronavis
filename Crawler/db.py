89737debd68a8474568b6fc0514f9402b53131d0
Wolfgang Jentner <wolfgang.jentner@uni-konstanz.de>
2020-05-07 21:39:04 +0200

    [#304] refactor crawler

--- Crawler/db.py
+++ Crawler/db.py
@@ -1,6 +1,5 @@
 import datetime
 
-import db_config
 import geojson
 from geoalchemy2 import Geometry
 from geoalchemy2.shape import to_shape
@@ -10,6 +9,8 @@ from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import (backref, deferred, relationship, scoped_session,
                             sessionmaker)
+
+import db_config
 
 Base = declarative_base()
 
@@ -207,6 +208,7 @@ class CasesLK(Base):
     death_rate = Column(Float(), nullable=False)
     cases_per_100k = Column(Float(), nullable=False)
     cases_per_population = Column(Float(), nullable=False)
+    cases7_per_100k = Column(Float(), nullable=False)
 
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
