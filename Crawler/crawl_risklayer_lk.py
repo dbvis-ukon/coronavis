87eb6993d68882d41cdfc3a01f2a749acd460580
Maximilian Fischer <max.fischer@uni-konstanz.de>
2020-12-07 15:40:55 +0100

    integrate RK updates

--- Crawler/crawl_risklayer_lk.py
+++ Crawler/crawl_risklayer_lk.py
@@ -96,26 +96,53 @@ if current_try > NUM_RETRIES or data is None:
 
 logger.info('Extract data')
 prognosis_today = data['Statistik Überblick'].iloc[16, 6]
+
+# old
 #df = data['Kreise Alt'].iloc[3:, [1, 2, 13, 8, 10, 25, 26, 27, 28, 29, 41, 42, 30, 31, 32, 33, 34, 35, 36, 37]]
-df = data['Kreise'].iloc[3:, [1, 2, 13, 8, 10]]
-df[1] = df[1].astype(str)
-df[1] = df[1].apply(lambda x: x.zfill(5))
-df[8] = df[8].apply(lambda x: x != '')
+#df = data['Kreise'].iloc[3:, [1, 2, 13, 8, 10]]
+#df[8] = df[8].apply(lambda x: x != '')
+
+
+# new again...
+#                              0 1  2  3  4  5  6  7  8   9
+df = data['Haupt'].iloc[5:406,[2,0, 0, 10,3,47,48,49, 15,39]] # AGS, Name, Name (->update-status) Today, -1d, -2d, -3d, -4d, death today, death -1d
+df[2] = df[2].astype(str)
+df[2] = df[2].apply(lambda x: x.zfill(5))
 db_array = df.to_numpy()
+
+# get update status via hack (because RK calc is complex)
+dfhelp = data['Kreise'].iloc[3:,[0,2,8]]
+dfhelp[0] = dfhelp[0] - 1
+dfhelp[8] = dfhelp[8].apply(lambda x: x != '' )
+db_help_array = dfhelp.to_numpy()
+db_help_array = db_help_array[db_help_array[:,0].argsort()] #sort like the main table
+
+# check if alignment fits
+equal_positions = (db_help_array[:, 1] == db_array[:, 1]).sum() 
+if equal_positions >= 357:
+    pass # perfect, we expect that based on incorrect RK naming
+elif equal_positions >= 320: 
+    logger.warning(f"Aligning update status resulted in {equal_positions} matches, which is likely fine (we expect 357).")
+else:
+    logger.error(f"Aligning RK update status failed with {equal_positions} < 320 matches. RK might have changed their format again!")
+    exit(1)
+    
+# copy updated status in the correct ordering
+db_array[:,2] = db_help_array[:,2]
 
 # reformat
 entries = []
 updated_today_count = 0
 time_23_59 = time(21, 59)
-date_arr = [{'datenbestand': current_update, 'row_id_cases': 4, 'row_id_deaths': 2},
-            # {'datenbestand': datetime.combine(current_update.date() - timedelta(days=1), time_23_59).replace(
-                # tzinfo=timezone.utc), 'row_id_cases': 5, 'row_id_deaths': 10},
-            # {'datenbestand': datetime.combine(current_update.date() - timedelta(days=2), time_23_59).replace(
-                # tzinfo=timezone.utc), 'row_id_cases': 6, 'row_id_deaths': 11},
-            # {'datenbestand': datetime.combine(current_update.date() - timedelta(days=3), time_23_59).replace(
-                # tzinfo=timezone.utc), 'row_id_cases': 7, 'row_id_deaths': None},
-            # {'datenbestand': datetime.combine(current_update.date() - timedelta(days=4), time_23_59).replace(
-                # tzinfo=timezone.utc), 'row_id_cases': 8, 'row_id_deaths': None},
+date_arr = [{'datenbestand': current_update, 'row_id_cases': 3, 'row_id_deaths': 8},
+             {'datenbestand': datetime.combine(current_update.date() - timedelta(days=1), time_23_59).replace(
+                 tzinfo=timezone.utc), 'row_id_cases': 4, 'row_id_deaths': 9},
+             {'datenbestand': datetime.combine(current_update.date() - timedelta(days=2), time_23_59).replace(
+                 tzinfo=timezone.utc), 'row_id_cases': 5, 'row_id_deaths': None},
+             {'datenbestand': datetime.combine(current_update.date() - timedelta(days=3), time_23_59).replace(
+                 tzinfo=timezone.utc), 'row_id_cases': 6, 'row_id_deaths': None},
+             {'datenbestand': datetime.combine(current_update.date() - timedelta(days=4), time_23_59).replace(
+                 tzinfo=timezone.utc), 'row_id_cases': 7, 'row_id_deaths': None},
             # {'datenbestand': datetime.combine(current_update.date() - timedelta(days=5), time_23_59).replace(
                 # tzinfo=timezone.utc), 'row_id_cases': 9, 'row_id_deaths': None},
             # {'datenbestand': datetime.combine(current_update.date() - timedelta(days=6), time_23_59).replace(
@@ -143,14 +170,20 @@ for row in db_array:
             'ags': row[0],
             'cases': row[history['row_id_cases']],
             'deaths': row[history['row_id_deaths']] if history['row_id_deaths'] is not None else None,
-            'updated_today': row[3] or (history['datenbestand'] != current_update)
+            'updated_today': row[2] or (history['datenbestand'] != current_update)
         }
-        if (history['datenbestand'] == current_update) and row[4]:
+        if (history['datenbestand'] == current_update) and row[2]:
             updated_today_count += 1
         if (isinstance(entry['cases'], int) or entry['cases'] == None) and (isinstance(entry['deaths'], int) or entry['deaths'] == None):
             entries.append(entry)
         else:
             logger.warning(f"Could not parse cases or deaths of {entry} correctly. Will omit this entry.")
+
+# debug
+#print(updated_today_count)
+#import pprint
+#pprint.pprint(entries)
+#exit()
 
 try:
     conn, cur = get_connection()
