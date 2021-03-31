4cc6e2c5dfcbbe9ef4d6151ff612d3bac3f9659a
Wolfgang Jentner <wolfgang.jentner@uni-konstanz.de>
2021-03-31 01:36:13 +0000

    don't print config

--- Backend/gunicorn.conf.py
+++ Backend/gunicorn.conf.py
@@ -4,7 +4,8 @@ bind = "0.0.0.0:5000"
 workers = multiprocessing.cpu_count() * 2 + 1
 threads = multiprocessing.cpu_count() * 2 + 1
 
-print_config = True
+# fails with gunicorn v20.1.0
+# print_config = True
 
 accesslog = '-'
 
