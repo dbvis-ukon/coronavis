4f66a0192562364ae17194a34df2772985d27b25
Wolfgang Jentner <wolfgang.jentner@uni-konstanz.de>
2021-04-05 17:44:59 +0200

    [#401] remove debug

--- Backend/prometheus.py
+++ Backend/prometheus.py
@@ -3,8 +3,6 @@ import os
 from prometheus_flask_exporter import PrometheusMetrics
 from prometheus_flask_exporter.multiprocess import GunicornPrometheusMetrics
 
-print(os.environ.keys())
-
 # metrics = PrometheusMetrics.for_app_factory(path='/metrics')
 
 metrics = GunicornPrometheusMetrics.for_app_factory(path='/metrics')
