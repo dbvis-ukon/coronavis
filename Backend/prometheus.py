963bfe5aa8e0cb4a53003804da5c2a1a96eeaaa4
Wolfgang Jentner <wolfgang.jentner@uni-konstanz.de>
2021-04-05 18:25:19 +0200

    [#401] use internal

--- Backend/prometheus.py
+++ Backend/prometheus.py
@@ -1,11 +1,11 @@
 import os
 
 from prometheus_flask_exporter import PrometheusMetrics
-from prometheus_flask_exporter.multiprocess import GunicornPrometheusMetrics
+from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMetrics
 
 is_gunicorn = "gunicorn" in os.environ.get("SERVER_SOFTWARE", "")
 
 if is_gunicorn:
-    metrics = GunicornPrometheusMetrics.for_app_factory()
+    metrics = GunicornInternalPrometheusMetrics.for_app_factory()
 else:
     metrics = PrometheusMetrics.for_app_factory()
