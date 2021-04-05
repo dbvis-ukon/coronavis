6e17db8147b5126a139ace621b9b5ea28a865271
Wolfgang Jentner <wolfgang.jentner@uni-konstanz.de>
2021-04-05 18:08:32 +0200

    [#401] leave defaults

--- Backend/prometheus.py
+++ Backend/prometheus.py
@@ -3,6 +3,9 @@ import os
 from prometheus_flask_exporter import PrometheusMetrics
 from prometheus_flask_exporter.multiprocess import GunicornPrometheusMetrics
 
-# metrics = PrometheusMetrics.for_app_factory(path='/metrics')
+is_gunicorn = "gunicorn" in os.environ.get("SERVER_SOFTWARE", "")
 
-metrics = GunicornPrometheusMetrics.for_app_factory(path='/metrics')
+if is_gunicorn:
+    metrics = GunicornPrometheusMetrics.for_app_factory()
+else:
+    metrics = PrometheusMetrics.for_app_factory()
