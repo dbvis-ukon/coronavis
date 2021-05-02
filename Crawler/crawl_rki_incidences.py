4dd00f78fe8935a7e6734c4d981390fd58958b6b
Wolfgang Jentner <wolfgang.jentner@uni-konstanz.de>
2021-05-02 14:07:36 +0200

    [hotfix] crawler (fix): handle nan correctly

--- Crawler/crawl_rki_incidences.py
+++ Crawler/crawl_rki_incidences.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 # author: Wolfgang Jentner
+import math
 import os
 import sys
 import datetime
@@ -109,7 +110,7 @@ def parse_and_insert_sheet(df, sheet_name, col_name):
     entries = []
     for r_idx in range(1, data.shape[0]):
         for c_idx in range(1, data.shape[1]):
-            val = None if data.iloc[r_idx, c_idx] == '' else data.iloc[r_idx, c_idx]
+            val = None if data.iloc[r_idx, c_idx] == '' or math.isnan(data.iloc[r_idx, c_idx]) else data.iloc[r_idx, c_idx]
             entry = {
                 'datenbestand': datenbestand,
                 'id': "{:05d}".format(data.iloc[r_idx, 0]),
