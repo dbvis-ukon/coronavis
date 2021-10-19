9d251ccc516a9fd6bceb004941f64537973841a6
Wolfgang Jentner <wolfgang.jentner@uni-konstanz.de>
2021-10-19 11:39:05 +0000

    [] backend (refactoring): change pool params for sql connection

--- Backend/db.py
+++ Backend/db.py
@@ -1,4 +1,4 @@
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy(engine_options={"pool_size": 10, "max_overflow": 50})
+db = SQLAlchemy(engine_options={"pool_size": 5, "max_overflow": 20})
 """:type: sqlalchemy.orm.Session"""
