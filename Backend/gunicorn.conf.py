ebb80831c07d460b6665d632401f87d01aaa222e
Wolfgang Jentner <wolfgang.jentner@uni-konstanz.de>
2021-10-20 16:58:23 +0000

    [#418] reduce workers and threads for non-production builds

--- Backend/gunicorn.conf.py
+++ Backend/gunicorn.conf.py
@@ -1,8 +1,19 @@
 import multiprocessing
+import os
+
+
+ENVIRONMENT = os.environ.get('ENVIRONMENT').replace('\n', '') if os.environ.get('ENVIRONMENT') else 'development'
 
 bind = "0.0.0.0:5000"
-workers = multiprocessing.cpu_count() * 2 + 1
-threads = multiprocessing.cpu_count() * 2 + 1
+
+if ENVIRONMENT == 'production':
+    workers = multiprocessing.cpu_count() * 2 + 1
+    threads = multiprocessing.cpu_count() * 2 + 1
+else:
+    workers = 1
+    threads = 1
+
+print(f'launching server with {workers} workers and {threads} threads')
 
 # fails with gunicorn v20.1.0
 # print_config = True
